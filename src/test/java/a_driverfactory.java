5a311273b0cab759b00e8729b1912f9294600e17
Richard Bradshaw <richardbradshaw@gmail.com>
2015-05-04 18:17:53 +0100

    Driver class done

---
+++ src/test/java/a_driverfactory.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by IEUser on 04/05/2015.
+ */
+public class a_driverfactory {
+    
+    @Test
+    public void A_CreateFirefoxDriver()
+    {
+        FirefoxDriver Driver = new FirefoxDriver();
+        //Driver remains open, how do we stop that?
+    }
+
+    @Test
+    public void A_CreateChromeDriver()
+    {
+        ChromeDriver Driver = new ChromeDriver();
+        //Why doesn't this work?
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\IEUser\\Documents\\RTDWebDriverWorkshop\\chromedriver.exe");
+        //Mac? Place chromedriver executable in this foler /usr/bin, download mac version though, http://chromedriver.storage.googleapis.com/index.html?path=2.15/
+    }
+
+    @Test
+    public void A_CreateInternetExplorerDriver()
+    {
+        //System.setProperty("webdriver.ie.driver", "C:\\Users\\IEUser\\Documents\\RTDWebDriverWorkshop\\IEDriverServer.exe");
+        InternetExplorerDriver Driver = new InternetExplorerDriver();
+    }
+
+    public void A_CreateRemoteWebDriver() throws MalformedURLException {
+        RemoteWebDriver Driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+    }
+}
