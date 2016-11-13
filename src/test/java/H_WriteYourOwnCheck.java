3897dd289c93d24ac5de9208942ec7390fdf0ca4
Richard Bradshaw <richardbradshaw@gmail.com>
2016-11-13 23:03:23 +0000

    new challenge

---
+++ src/test/java/H_WriteYourOwnCheck.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by richard on 13/11/2016.
+ */
+public class H_WriteYourOwnCheck
+{
+    @Test
+    public void h_YourOwnCheck()
+    {
+        //Pick a site and see if you can create some exmaple checks
+    }
+}
